0ee9d6f5c98c7208335f901f63e0eb006baf20b3
Nithish R <nithishr@gmail.com>
2016-12-03 12:15:21 +0100

    Basic Structure

--- loveHackathon.py
+++ loveHackathon.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, render_template_string
 
 app = Flask(__name__)
 
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/home')
+def home():
+    return render_template_string('Home {{name}}', name='admin')
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',debug=True)
